539020be200321c643e0b821a5e5d612a7c98635
Angelica <Angelica137@users.noreply.github.com>
2020-09-22 20:34:44 +0100

    amend comment in settings

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
     """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialise the game's settings.""""
+        """Initialise the game's settings."""
         # Screen settings
         self.scrren_width = 1200
         self.screen_heigh = 800
