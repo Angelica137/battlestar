1db62ee1167c63c8ce74dfaa7f0ce76a334c37ef
Angelica <Angelica137@users.noreply.github.com>
2020-09-22 19:40:28 +0100

    amend typo

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,3 +21,9 @@ class AlienInvasion:
 
             # Make the most recently drawn screens visible.
             pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
