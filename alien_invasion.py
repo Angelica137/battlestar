ebdba7cba71ba071869d02a9c1b1c77079a80716
Angelica <Angelica137@users.noreply.github.com>
2020-09-24 22:47:32 +0100

    create fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -58,14 +58,12 @@ class AlienInvasion:
                 self._create_alien(alien_number, row_number)
 
     def _create_alien(self, alien_number, row_number):
-            """Create an alien and place it in the row"""
-            alien = Alien(self)
-            alien_width, alien_height = alien.rect.size
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
-            self.aliens.add(alien)
-
+        """Create an alien and place it in the row"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_bullets(self):
