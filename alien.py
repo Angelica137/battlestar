a895ed04fb6d9490d8d6a4a10add8ce4142b1209
Angelica <Angelica137@users.noreply.github.com>
2020-09-24 23:07:35 +0100

    add update method to alien

--- alien.py
+++ alien.py
@@ -21,3 +21,8 @@ class Alien(Sprite):
 
         # Store the alien's exact horizontal position
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the alien to the right."""
+        self.x += self.settings.alien_speed
+        self.rect.x = self.x
