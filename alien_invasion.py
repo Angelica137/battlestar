e6ae303d2c2284179c9d9d7185a79c5c3ede9081
Angelica <Angelica137@users.noreply.github.com>
2020-09-24 21:42:29 +0100

    draw alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -91,6 +91,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
