fcbf96b1d9e0e4ce09db7552c8e32ea0b1322c26
Angelica <Angelica137@users.noreply.github.com>
2020-09-25 21:48:08 +0100

    draw button when game is not active

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,7 @@ from ship import Ship
 from bullet import Bullet
 from alien import Alien
 from game_stats import GameStats
+from button import Button
 
 
 class AlienInvasion:
@@ -30,6 +31,8 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+
+        self.play_button = Button(self, "Play")
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -192,6 +195,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Draw the play button when game is not active
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         pygame.display.flip()
 
