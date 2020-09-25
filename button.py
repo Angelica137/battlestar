0a0ff733af12831d30d69cb2e66a291091839037
Angelica <Angelica137@users.noreply.github.com>
2020-09-25 21:19:11 +0100

    build button

--- button.py
+++ button.py
@@ -13,3 +13,7 @@ class Button:
     self.button_color = (0, 255, 0)
     self.text_color = (255, 255, 255)
     self.font = pygame.font.SysFont(None, 48)
+
+    # Build button
+    self.rect = pygame.Rect(0, 0, self.width, self.height)
+    self.rect.center = self.screen_rect.center
