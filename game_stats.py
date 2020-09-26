ce9fabd5d60ecf2dc6d38f9468786f1d6a437fad
Angelica <Angelica137@users.noreply.github.com>
2020-09-26 15:04:51 +0100

    keep high score

--- game_stats.py
+++ game_stats.py
@@ -9,6 +9,9 @@ class GameStats:
         # Set game active flag
         self.game_active = False
 
+        # Keep game high score
+        self.high_score = 0
+
     def reset_stats(self):
         """Initialise statistics that can change during the game"""
         self.ships_left = self.settings.ship_limit
