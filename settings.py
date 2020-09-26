f4d071271d1b8df8d70f8cd540e14a24a17813e0
Angelica <Angelica137@users.noreply.github.com>
2020-09-26 14:18:53 +0100

    add score value to alien

--- settings.py
+++ settings.py
@@ -36,6 +36,9 @@ class Settings:
         # change fleet direction
         self.fleet_direction = 1
 
+        # Scoring
+        self.alien_points = 50
+
     def increase_speed(self):
         """Increase speed settings"""
         self.ship_speed *= self.speedup_scale
